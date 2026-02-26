ea145857b3235f1db4f4ac3aa74300f1d1ffdfaa
daimaobobocha <zixuan_wang2006@163.com>
2026-02-25 20:41:20 -0700

    Godot

---
+++ godot/area_3d.gd
@@ -0,0 +1,12 @@
+extends Area3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	body_entered.connect(_on_body_entered)
+
+func _on_body_entered(body: Node) -> void:
+	if body.is_in_group("Player"):
+		print("Coin collected!")
+		queue_free()
+	
